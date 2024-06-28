732a3d1a1ce63a135bea83d2dad0b670efb62780
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-28 21:25:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// write code here
+let t1 = 'Welcome to Thailand'
+let t2 = 'Don\'n be afraid\n'
+let t3 = 'สวัสดีประเทศไทย'
+console.log(t1.length)//19
+console.log(t2.length)//16
+console.log(t3.length)//15
